c1814f31e970af6677afd31bee5560a062ebe8a7
audioanton <antonlarsson.musik@gmail.com>
2025-02-17 11:07:14 +0100

    commit

--- src/test/java/functional_programming/MovieUtilTest.java
+++ src/test/java/functional_programming/MovieUtilTest.java
@@ -4,17 +4,18 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.Assert.*;
 public class MovieUtilTest {
     MovieUtil m = new MovieUtil();
     List<Movie> testMovies = MovieUtil.getTestMovieList();
     List<Movie> emptyTest = new ArrayList<>();
+    Movie testMovie = new Movie("1", "1", 1, List.of("1", "1"), "1", List.of("1", "1"), 1, List.of("1", "1"), 1);
 
     @Test
     public void getTestMovieList() {
         List<Movie> test = MovieUtil.getTestMovieList();
-
         assertNotNull(test);
         assertEquals(5, test.size());
         assertEquals("Rambo", test.getFirst().getTitle());
@@ -27,29 +28,33 @@ public class MovieUtilTest {
     public void countMovies() {
         assertEquals(5, m.countMovies(testMovies));
         assertEquals(0, m.countMovies(emptyTest));
+        assertEquals(1, m.countMovies(List.of(testMovie)));
     }
 
     @Test
     public void longestMovie() {
         assertEquals(0, m.longestMovie(emptyTest));
         assertEquals(210, m.longestMovie(testMovies));
+        assertEquals(1, m.longestMovie(List.of(testMovie, testMovie)));
     }
-
 
     @Test
     public void countDistinctGenres() {
         assertNotEquals(12, m.countDistinctGenres(testMovies));
         assertEquals(6, m.countDistinctGenres(testMovies));
         assertEquals(0, m.countDistinctGenres(emptyTest));
+        assertEquals(1, m.countDistinctGenres(List.of(testMovie, testMovie)));
     }
 
     @Test
     public void getActorsInBestMovie() {
         List<String> actors = m.getActorsInBestMovie(testMovies);
         assertEquals("Bingo", actors.getFirst());
-        assertEquals("Ufo", actors.getLast());
-        assertEquals(2, actors.size());
+        assertEquals("Statist", actors.getLast());
+        assertEquals(3, actors.size());
         assertFalse(actors.contains("Hermione"));
+
+        //Test om fler filmer har samma betyg
     }
 
     @Test
@@ -60,17 +65,29 @@ public class MovieUtilTest {
 
     @Test
     public void countActorsFeaturedInMultipleMovies() {
+        assertEquals(2, m.countActorsFeaturedInMultipleMovies(testMovies));
+        assertEquals(0, m.countActorsFeaturedInMultipleMovies(emptyTest));
     }
 
     @Test
     public void getActorMostFeatured() {
+        assertNotEquals("Pistoler", m.getActorMostFeatured(testMovies));
+        assertEquals("Statist", m.getActorMostFeatured(testMovies));
+        assertThrows(NoSuchElementException.class, () -> m.getActorMostFeatured(emptyTest));
     }
 
     @Test
     public void countDistinctLanguages() {
+        assertNotEquals(13, m.countDistinctLanguages(testMovies));
+        assertEquals(5, m.countDistinctLanguages(testMovies));
+        assertEquals(0, m.countDistinctLanguages(emptyTest));
     }
 
     @Test
     public void existsDuplicateTitle() {
+        assertFalse(m.existsDuplicateTitle(testMovies));
+        testMovies.add(new Movie("5", "title", 1, null, "director", null, 1, null, 1));
+        testMovies.add(new Movie("5", "title", 1, null, "director", null, 1, null, 1));
+        assertTrue(m.existsDuplicateTitle(testMovies));
     }
 }
