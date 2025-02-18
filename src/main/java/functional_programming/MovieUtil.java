6b561e6bd88d461db6a2dadc97e2b9daac6eeb10
audioanton <antonlarsson.musik@gmail.com>
2025-02-18 13:14:08 +0100

    updated tests and added higher order functions

--- src/main/java/functional_programming/MovieUtil.java
+++ src/main/java/functional_programming/MovieUtil.java
@@ -1,6 +1,7 @@
 package functional_programming;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class MovieUtil {
@@ -11,23 +12,50 @@ public class MovieUtil {
     }
 
 // Hitta längden på den film som var längst (högst runtime). Returnera ett tal.
-    public int longestMovie(List<Movie> movies) {
-        return movies.stream().map(Movie::getRuntime).max(Integer::compareTo).orElse(0);
+//    Higher Order Function
+    public int highestIntValueInMovie(List<Movie> movies, Function<Movie, Integer> getValue) {
+        return movies.stream().map(getValue).max(Integer::compareTo).orElse(0);
     }
 
 // Hur många UNIKA genrer hade filmerna från 1975. Returnera ett tal.
-    public long countDistinctGenres(List<Movie> movies) {
-        return movies.stream().flatMap(m -> m.getGenres().stream()).distinct().count();
+// Hur många UNIKA språk har filmerna? Returnera ett tal.
+//    Higher Order Function
+    public long countDistinctInFlatMap(List<Movie> movies, Function<Movie, List<String>> function) {
+        return movies.stream().flatMap(m -> function.apply(m).stream()).distinct().count();
+    }
+
+    public List<String> getActorsMostFeatured(List<Movie> movies) {
+        return movies.stream().flatMap(m -> m.getCast().stream())
+                .collect(Collectors.groupingBy(name -> name, Collectors.counting()))
+                .entrySet().stream()
+                .collect(Collectors.groupingBy(Map.Entry::getValue, Collectors.mapping(Map.Entry::getKey, Collectors.toList())))
+                .entrySet().stream()
+                .max(Comparator.comparingLong(Map.Entry::getKey))
+                .map(Map.Entry::getValue)
+                .orElse(Collections.emptyList());
+    }
+
+    // Vad hette den skådis som var med i flest filmer? Returnera en String
+    public String getActorMostFeatured(List<Movie> movies) {
+        return movies.stream().flatMap(m -> m.getCast().stream())
+                .collect(Collectors.groupingBy(name -> name, Collectors.counting()))
+                .entrySet().stream()
+                .max(Comparator.comparingLong(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
+                .orElse("None");
     }
 
 // Vilka skådisar som spelade i den film som hade högst imdb-rating. Returnera en
 //    List<String> med deras namn.
     public List<String> getActorsInBestMovie(List<Movie> movies) {
-//        movies.stream().collect(Collectors.groupingBy(Movie::getImdbRating, Collectors.m),
-
-//        movies.stream().max(Comparator.comparingDouble(Movie::getImdbRating)).collect(Collectors.groupingBy(Movie::getImdbRating, Collectors.)).values().stream().filter()
-
-        return movies.stream().reduce((a, b) -> a.getImdbRating() > b.getImdbRating() ? a : b).get().getCast();
+        return movies.stream().collect(Collectors.groupingBy(Movie::getImdbRating))
+                .entrySet().stream()
+                .max(Comparator.comparingDouble(Map.Entry::getKey))
+                .map(Map.Entry::getValue).orElse(Collections.emptyList())
+                .stream().flatMap(movie -> movie.getCast().stream())
+                .toList();
+//        following returns only one movies cast..
+//        return movies.stream().reduce((a, b) -> a.getImdbRating() > b.getImdbRating() ? a : b).get().getCast();
 }
 
 // Vad är titeln på den film som hade minst antal skådisar listade? Returnera en String.
@@ -43,34 +71,9 @@ public class MovieUtil {
                 .count();
     }
 
-// Vad hette den skådis som var med i flest filmer? Returnera en String
-    public String getActorMostFeatured(List<Movie> movies) {
-        return movies.stream().flatMap(m -> m.getCast().stream())
-                .collect(Collectors.groupingBy(s -> s, Collectors.counting()))
-                .entrySet().stream()
-                .max(Comparator.comparingLong(entry -> entry.getValue()))
-                .orElseThrow()
-                .getKey();
-    }
-
-// Hur många UNIKA språk har filmerna? Returnera ett tal.
-    public long countDistinctLanguages(List<Movie> movies) {
-        return movies.stream().flatMap(m -> m.getLanguages().stream()).distinct().count();
-    }
-
 // Finns det någon titel som mer än en film har? Returnera en bool.
     public boolean existsDuplicateTitle(List<Movie> movies) {
         return movies.stream().collect(Collectors.groupingBy(Movie::getTitle, Collectors.counting()))
                 .values().stream().anyMatch(i -> i > 1);
-    }
-
-    public static List<Movie> getTestMovieList() {
-        List<Movie> movies = new ArrayList<>();
-        movies.add(new Movie("1", "Rambo", 1999, List.of("drama", "romcom"), "Lasse", List.of("Sylvester"), 10.0, List.of("English", "Svenska", "Danish"), 103));
-        movies.add(new Movie("2", "Harry Potter", 2003, List.of("fantasy", "romcom"), "Jessica", List.of("Hermione", "Statist", "Pistoler"), 10.0, List.of("English", "Svenska", "Chinese"), 15));
-        movies.add(new Movie("3", "Dumbo", 1963, List.of("drama", "romcom", "animation"), "Lasse", List.of("Elefanto", "Statist", "Pistoler"), 100.0, List.of("English", "Svenska", "Francaise"), 210));
-        movies.add(new Movie("4", "Alien", 1999, List.of("horror, romcom"), "Jessica", List.of("Bingo", "Ufo", "Statist"), 1000.0, List.of("English", "Svenska"), 143));
-        movies.add(new Movie("5", "Wall-E", 1999, List.of("drama", "romcom", "action"), "Lasse", List.of("Robot", "Eva"), 50.43, List.of("English", "Svenska"), 67));
-        return movies;
     }
 }
--- src/test/java/functional_programming/MovieUtilTest.java
+++ src/test/java/functional_programming/MovieUtilTest.java
@@ -3,26 +3,23 @@ package functional_programming;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.*;
 public class MovieUtilTest {
     MovieUtil m = new MovieUtil();
-    List<Movie> testMovies = MovieUtil.getTestMovieList();
-    List<Movie> emptyTest = new ArrayList<>();
+    List<Movie> testMovies = List.of(
+            new Movie("1", "Rambo", 1999, List.of("drama", "romcom"), "Lasse", List.of("Sylvester"), 10.0, List.of("English", "Svenska", "Danish"), 103),
+            new Movie("2", "Harry Potter", 2003, List.of("fantasy", "romcom"), "Jessica", List.of("Hermione", "Statist", "Pistoler"), 10.0, List.of("English", "Svenska", "Chinese"), 15),
+            new Movie("3", "Dumbo", 1963, List.of("drama", "romcom", "animation"), "Lasse", List.of("Elefanto", "Statist", "Pistoler"), 100.0, List.of("English", "Svenska", "Francaise"), 210),
+            new Movie("4", "Alien", 1999, List.of("horror, romcom"), "Jessica", List.of("Bingo", "Ufo", "Statist"), 1000.0, List.of("English", "Svenska"), 143),
+            new Movie("5", "Wall-E", 1999, List.of("drama", "romcom", "action"), "Lasse", List.of("Robot", "Eva"), 50.43, List.of("English", "Svenska"), 67)
+    );
+    List<Movie> emptyTest = Collections.emptyList();
     Movie testMovie = new Movie("1", "1", 1, List.of("1", "1"), "1", List.of("1", "1"), 1, List.of("1", "1"), 1);
 
-    @Test
-    public void getTestMovieList() {
-        List<Movie> test = MovieUtil.getTestMovieList();
-        assertNotNull(test);
-        assertEquals(5, test.size());
-        assertEquals("Rambo", test.getFirst().getTitle());
-        assertEquals("Wall-E", test.getLast().getTitle());
-        assertEquals("Lasse", test.getFirst().getDirector());
-        assertEquals(3,test.getLast().getGenres().size());
-    }
 
     @Test
     public void countMovies() {
@@ -32,18 +29,28 @@ public class MovieUtilTest {
     }
 
     @Test
-    public void longestMovie() {
-        assertEquals(0, m.longestMovie(emptyTest));
-        assertEquals(210, m.longestMovie(testMovies));
-        assertEquals(1, m.longestMovie(List.of(testMovie, testMovie)));
+    public void highestIntValueInMovie() {
+//        longest runtime
+        assertEquals(0, m.highestIntValueInMovie(emptyTest, Movie::getRuntime));
+        assertEquals(210, m.highestIntValueInMovie(testMovies, Movie::getRuntime));
+        assertEquals(1, m.highestIntValueInMovie(List.of(testMovie, testMovie), Movie::getRuntime));
+
+//        number of largest cast
+        assertEquals(3, m.highestIntValueInMovie(testMovies, m -> m.getCast().size()));
+//        most characters in title
+        assertEquals(12, m.highestIntValueInMovie(testMovies, m -> m.getTitle().length()));
     }
 
     @Test
-    public void countDistinctGenres() {
-        assertNotEquals(12, m.countDistinctGenres(testMovies));
-        assertEquals(6, m.countDistinctGenres(testMovies));
-        assertEquals(0, m.countDistinctGenres(emptyTest));
-        assertEquals(1, m.countDistinctGenres(List.of(testMovie, testMovie)));
+    public void countDistinctInFlatMap() {
+        assertNotEquals(12, m.countDistinctInFlatMap(testMovies, Movie::getGenres));
+        assertEquals(6, m.countDistinctInFlatMap(testMovies, Movie::getGenres));
+        assertEquals(0, m.countDistinctInFlatMap(emptyTest, Movie::getGenres));
+        assertEquals(1, m.countDistinctInFlatMap(List.of(testMovie, testMovie), Movie::getGenres));
+
+        assertNotEquals(13, m.countDistinctInFlatMap(testMovies, Movie::getLanguages));
+        assertEquals(5, m.countDistinctInFlatMap(testMovies, Movie::getLanguages));
+        assertEquals(0, m.countDistinctInFlatMap(emptyTest, Movie::getLanguages));
     }
 
     @Test
@@ -54,7 +61,13 @@ public class MovieUtilTest {
         assertEquals(3, actors.size());
         assertFalse(actors.contains("Hermione"));
 
-        //Test om fler filmer har samma betyg
+        List<Movie> movies = new ArrayList<>(testMovies);
+
+        movies.add(new Movie("7", "Kopps", 1999, List.of("horror, romcom"), "Jessica", List.of("Anton", "Bert", "Fisk"), 1000.0, List.of("English", "Svenska"), 143));
+        List<String> actorsTwo = m.getActorsInBestMovie(movies);
+        assertEquals(6, actorsTwo.size());
+        assertEquals("Fisk", actorsTwo.getLast());
+        assertEquals(0, m.getActorsInBestMovie(emptyTest).size());
     }
 
     @Test
@@ -73,21 +86,38 @@ public class MovieUtilTest {
     public void getActorMostFeatured() {
         assertNotEquals("Pistoler", m.getActorMostFeatured(testMovies));
         assertEquals("Statist", m.getActorMostFeatured(testMovies));
-        assertThrows(NoSuchElementException.class, () -> m.getActorMostFeatured(emptyTest));
+        assertEquals("None", m.getActorMostFeatured(emptyTest));
     }
 
     @Test
-    public void countDistinctLanguages() {
-        assertNotEquals(13, m.countDistinctLanguages(testMovies));
-        assertEquals(5, m.countDistinctLanguages(testMovies));
-        assertEquals(0, m.countDistinctLanguages(emptyTest));
+    public void getActorsMostFeatured() {
+        assertEquals("Statist", m.getActorsMostFeatured(testMovies).getFirst());
+        assertEquals(1, m.getActorsMostFeatured(testMovies).size());
+        assertEquals(0, m.getActorsMostFeatured(emptyTest).size());
+
+        List<Movie> movies = new ArrayList<>(testMovies);
+        String newActor = "Kalle Anka";
+        String otherActor = "Mimmi Pigg";
+
+        IntStream.range(10,15).forEach(i ->
+                movies.add(new Movie(String.valueOf(i), "Kvack!", i, null, null, List.of(newActor), 100, null, 100))
+        );
+        assertEquals(newActor, m.getActorsMostFeatured(movies).getFirst());
+        assertEquals(1, m.getActorsMostFeatured(movies).size());
+
+        IntStream.rangeClosed(16, 20).forEach(i ->
+                movies.add(new Movie(String.valueOf(i), "Kvack!", i, null, null, List.of(otherActor), 100, null, 100))
+        );
+        assertEquals(2, m.getActorsMostFeatured(movies).size());
     }
 
     @Test
     public void existsDuplicateTitle() {
         assertFalse(m.existsDuplicateTitle(testMovies));
-        testMovies.add(new Movie("5", "title", 1, null, "director", null, 1, null, 1));
-        testMovies.add(new Movie("5", "title", 1, null, "director", null, 1, null, 1));
-        assertTrue(m.existsDuplicateTitle(testMovies));
+        List<Movie> movies = new ArrayList<>(testMovies);
+        movies.add(new Movie("5", "title", 1, null, "director", null, 1, null, 1));
+        movies.add(new Movie("5", "title", 1, null, "director", null, 1, null, 1));
+        assertTrue(m.existsDuplicateTitle(movies));
     }
+
 }
